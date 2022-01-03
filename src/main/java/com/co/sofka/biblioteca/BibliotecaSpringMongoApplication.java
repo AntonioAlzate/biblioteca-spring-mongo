9b757164892de1f16c5c9e5930bf9d69f2a4f650
Antonio Alzate <antonioalzate7@gmail.com>
2022-01-03 10:06:54 -0500

    Commit inicial.

---
+++ src/main/java/com/co/sofka/biblioteca/BibliotecaSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.co.sofka.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSpringMongoApplication.class, args);
+	}
+
+}
