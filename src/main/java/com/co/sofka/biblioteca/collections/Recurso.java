99d4900956f936fdab2a704017e1dcffc53c5aeb
Antonio Alzate <antonioalzate7@gmail.com>
2022-01-03 10:17:12 -0500

    Creación de la entidad recurso.

---
+++ src/main/java/com/co/sofka/biblioteca/collections/Recurso.java
@@ -0,0 +1,62 @@
+package com.co.sofka.biblioteca.collections;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document(collection = "recursos")
+public class Recurso {
+
+    @Id
+    private String id = UUID.randomUUID().toString().substring(0, 10);
+
+    private String tipo;
+
+    private String tematica;
+
+    private LocalDateTime fechaPrestamo;
+
+    private Boolean disponibilidad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTematica() {
+        return tematica;
+    }
+
+    public void setTematica(String tematica) {
+        this.tematica = tematica;
+    }
+
+    public LocalDateTime getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDateTime fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Boolean getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(Boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+}
